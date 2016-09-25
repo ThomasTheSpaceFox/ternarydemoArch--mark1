74cf5d3fb4a87304bfa3d832fc2cd3ecb699460d
thomasthespacefox <teleathers22@gmail.com>
2016-09-25 23:46:59 +0000

    add better graphics, add various status displays

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 (Ternary Demo Arch. Mark 1.)
 a simple and somewhat technical Balanced ternary Virtual Machine.
 
-v1.1 alpha
+v1.2 alpha
 
 (c)2016 Thomas Leathers
 
--- TDA_MK1.py
+++ TDA_MK1.py
@@ -18,11 +18,16 @@ pygame.display.init()
 screensurf=pygame.display.set_mode((648, 486))
 pygame.display.set_caption("TDA Mark 1", "TDA Mark 1")
 pygame.font.init()
+#used for TTY
 simplefont = pygame.font.SysFont(None, 16)
-pixcnt1=0
+#used for smaller data displays (inst. data etc.)
+smldispfont = pygame.font.SysFont(None, 16)
+#used in larger data displays (register displays, etc.)
+lgdispfont = pygame.font.SysFont(None, 20)
+pixcnt1=40
 pixjmp=14
-
-abt=["TDA", "Mark 1", "v1.1", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "ready", ""]
+vmbg=pygame.image.load(os.path.join('GFX', 'VMBG.png'))
+abt=["TDA", "Mark 1", "v1.2", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "ready", ""]
 
 pygame.mixer.init(frequency=22050 , size=-16)
 
@@ -49,6 +54,9 @@ execfile('BOOTUP.CFG')
 RAMbank = {}
 
 #calmlst = open("ORDEREDLIST6.txt")
+screensurf.fill((0,127,255))
+screensurf.blit(vmbg, (0, 0))
+pygame.display.update()
 
 #ramstart
 for ramadr in libTDAcommon.calmlst:
@@ -56,6 +64,8 @@ for ramadr in libTDAcommon.calmlst:
 	ramadr=ramadr.replace("\n", "")
 	RAMbank[ramadr] = "000000"
 	#
+
+
 
 #RAMBANK startup end
 
@@ -69,6 +79,31 @@ REG2="000000"
 while stopflag==0:
 	curinst=(libtrom.tromreadinst(EXECADDR,ROMFILE))
 	curdata=(libtrom.tromreaddata(EXECADDR,ROMFILE))
+	#some screen display stuff & general blitting
+	#screensurf.fill((0,127,255))
+	#draw Background
+	screensurf.blit(vmbg, (0, 0))
+	#these show the instruction and data in the instruction/data box :)
+	insttext=smldispfont.render(curinst, True, (0, 255, 255), (0, 0, 0))
+	datatext=smldispfont.render(curdata, True, (0, 255, 127), (0, 0, 0))
+	screensurf.blit(insttext, (552, 40))
+	screensurf.blit(datatext, (590, 40))
+	#these draw the register displays :)
+	reg1text=lgdispfont.render(REG1, True, (255, 0, 127), (0, 0, 0))
+	reg2text=lgdispfont.render(REG2, True, (255, 127, 0), (0, 0, 0))
+	screensurf.blit(reg1text, (558, 78))
+	screensurf.blit(reg2text, (558, 118))
+	#TTY drawer :)
+	for fnx in abt:
+		fnx=fnx.replace('\n', '')
+		abttext=simplefont.render(fnx, True, (0, 127, 255), (0, 0, 0))
+		screensurf.blit(abttext, (45, pixcnt1))
+		pixcnt1 += pixjmp
+	pixcnt1=40
+	
+	#aaaaannnnddd update display! :D
+	pygame.display.update()
+	
 	#ROM READ (first register)
 	if curinst=="----":
 		REG1=(libtrom.tromreaddata(EXECADDR,ROMFILE))
@@ -198,6 +233,28 @@ while stopflag==0:
 		EXECADDR=EXECADDRNEXT
 	if stopflag==1:
 		abt=libTDAcommon.abtslackline(abt, "Press enter to exit.")
+		screensurf.fill((0,127,255))
+		screensurf.blit(vmbg, (0, 0))
+	
+		#these show the instruction and data in the instruction/data box :)
+		insttext=smldispfont.render(curinst, True, (0, 255, 255), (0, 0, 0))
+		datatext=smldispfont.render(curdata, True, (0, 255, 127), (0, 0, 0))
+		screensurf.blit(insttext, (552, 40))
+		screensurf.blit(datatext, (590, 40))
+		#these draw the register displays :)
+		reg1text=lgdispfont.render(REG1, True, (255, 0, 127), (0, 0, 0))
+		reg2text=lgdispfont.render(REG2, True, (255, 127, 0), (0, 0, 0))
+		screensurf.blit(reg1text, (558, 78))
+		screensurf.blit(reg2text, (558, 118))
+		for fnx in abt:
+			fnx=fnx.replace('\n', '')
+			abttext=simplefont.render(fnx, True, (0, 127, 255), (0, 0, 0))
+			screensurf.blit(abttext, (45, pixcnt1))
+			pixcnt1 += pixjmp
+		pixcnt1=38
+	
+	
+	pygame.display.update()
 	#clear buffer secion of IObus
 	#this means: DONT USE THE BUFFER SECTION OF THE IObus AS RAM :|
 	#chklist = open("ORDEREDLIST6REGISTER.txt")
@@ -206,14 +263,8 @@ while stopflag==0:
 		ramadr=ramadr.replace("\n", "")
 		RAMbank[ramadr] = "000000"
 		#
-	screensurf.fill((0,127,255))
-	for fnx in abt:
-		fnx=fnx.replace('\n', '')
-		abttext=simplefont.render(fnx, True, (255, 255, 255), (0, 0, 0))
-		screensurf.blit(abttext, (0, pixcnt1))
-		pixcnt1 += pixjmp
-	pixcnt1=0
-	pygame.display.update()
+	
+	
 	time.sleep(CPUWAIT)
 	
 	evhappenflg2=0
